00414334b50637dcf37f1829ea4812691fe10172
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2020-03-23 13:42:23 +0100

    ajout des classes des capteurs (Switch / capteur de couleur / capteur de distance) + ajout dans la classe sensor

--- include/Sensor.hpp
+++ include/Sensor.hpp
@@ -9,6 +9,11 @@
 #include "mbed.h"
 #include "Odometer.hpp"
 
+#include "TCA9548.h"
+#include "TCS34725.h"
+#include <VL53L0X.h>
+#include <vector>
+
 typedef enum{
     ODOMETER_X,ODOMETER_Y,ODOMETER_ALPHA   
 } SensorDataId;
@@ -28,6 +33,24 @@ class Sensor {
         ~Sensor();/*!< destructeur de Sensor */
     private:
         Odometer odometer;
+        /* Crée le switch avec l'adresse de base 0x70 */
+        TCA9548 i2cMux;
+
+        
+
+        
+        
+        
+        /* Crée la capteur de couleur avec l'adresse de base 0x29 */
+        TCS34725 capteurCouleur;
+        /* Donnée régulièrement mis à jour si la couleur lue est rouge ou pas */
+        int isRed = -1;
+
+        /* Crée la capteur de distance avec l'adresse de base 0x29 */
+        VL53L0X capteurDistance;
+        /* Vecteur de donnée des distances régulièrement mis à jour */
+        std::vector<uint16_t> valCaptDist;
+
 
 };
 #endif
--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -14,6 +14,41 @@
  */
 Sensor::Sensor():odometer(PIN_ODOMETER_ARIGHT,PIN_ODOMETER_BRIGHT,PIN_ODOMETER_ALEFT,PIN_ODOMETER_BLEFT,PERIMETREWHEEL_RIGHT,PERIMETREWHEEL_LEFT,DISTCENTER2WHEEL,NBPULSE){
     
+    printf("INIT Sensor ! \n\r");
+
+    /* Init le vecteur qui contiendra les valeurs des capteurs de distance mis à jour régulièrement */
+    valCaptDist.resize(6,0);
+
+    int checkInit = 0;
+
+    /* Initilise les 6 capteurs de distances */
+    for (size_t i = 0 ; i < valCaptDist.size(); i++) {
+
+        /* Selectionne le canal */
+        i2cMux.ch(CH2 << i);
+        
+        /* Test si le capteur est branché */
+        if ( !capteurDistance.init() ) {
+            printf("FAILED TO INIT sensor dist (n = %d)\n\r", i); //Initialize device and check for errors
+
+            continue ;
+        } else {
+            
+            /* Test si le capteur est correctement init */
+            checkInit = capteurDistance.readRangeSingleMillimeters();
+            if (checkInit != 0){
+                printf("INIT OK sensor (n = %d) : ", i ); 
+                printf("Dist. sensor (n = %d) (mm) = %d - ", i, checkInit );
+            } else {
+                printf("FAILED TO INIT sensor dist (n = %d) - ", i); //Initialize device and check for errors
+            }
+        }
+        /* Met à jour le vecteur de donnée */
+        valCaptDist[i] = checkInit;
+        printf("Val = %d \n\r", valCaptDist[i] );
+    }
+    
+
 }
 
 double Sensor::getSensorData(SensorDataId sensorDataId){
